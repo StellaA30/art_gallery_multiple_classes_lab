fe3e8a80dccc328115d270d208877159ce00ca15
Stella Annor <stella.annor@icloud.com>
2023-03-29 15:04:32 +0100

    Artwork class and testing completed

--- src/main/java/org/example/Artwork.java
+++ src/main/java/org/example/Artwork.java
@@ -1,4 +1,36 @@
 package org.example;
 
 public class Artwork {
+    private String title;
+    private String artist;
+    private double price;
+    public Artwork(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.price = 0;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/test/java/ArtworkTest.java
+++ src/test/java/ArtworkTest.java
@@ -1,2 +1,49 @@
+import org.example.Artwork;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ArtworkTest {
+
+    Artwork artwork;
+    @BeforeEach
+    public void setUp(){
+        artwork = new Artwork("Mona Lisa", "Da Vinci");
+    }
+
+//    public void hasName(){
+//        assertThat(ginger.getName()).isEqualTo("Ginger");
+//    }
+    @Test
+    public void hasTitle(){
+        assertThat(artwork.getTitle()).isEqualTo("Mona Lisa");
+    }
+    @Test
+    public void hasArtist(){
+        assertThat(artwork.getArtist()).isEqualTo("Da Vinci");
+    }
+    @Test
+    public void hasPrice(){
+        assertThat(artwork.getPrice()).isEqualTo(0);
+    }
+
+    @Test
+    public void canSetTitle(){
+        artwork.setTitle("Starry night");
+        assertThat(artwork.getTitle()).isEqualTo("Starry night");
+    }
+    @Test
+    public void canSetArtist(){
+        artwork.setArtist("Picasso");
+        assertThat(artwork.getArtist()).isEqualTo("Picasso");
+    }
+
+    @Test
+    public void canSetPrice(){
+        artwork.setPrice(500);
+        assertThat(artwork.getPrice()).isEqualTo(500);
+    }
+
+
 }
